48b7ef278b6e7feda2f4438364c4e5ebc43ed88d
Frederico Bittencourt Roriz <frederoriz@outlook.com>
2026-06-06 17:06:27 -0300

    Melhorias nos filtros e selects

--- contracts-dashboard/src/App.tsx
+++ contracts-dashboard/src/App.tsx
@@ -5,15 +5,26 @@ import ContractCard from './components/ContractCard'
 
 export default function App() {
 
-  type StatusFilter = '' | 'ativo' | 'cancelado'
-  type Order = 'id_desc' | 'id_asc' | 'asc_valor' | 'desc_valor' | 'asc_alfabeto' | 'desc_alfabeto'
+  type StatusFilter = '' | 'ativo' | 'cancelado';
+  type Order = number;
+  type statusOptions = string;
 
-  const [statusFilter, setStatusFilter] =
-    useState<StatusFilter>('')
+  const [statusFilter, setStatusFilter] = useState<StatusFilter>('');
+  const [search, setSearch] = useState('');
+  const [order, setOrder] = useState<Order>(1);
+  const orderOptions = [
+    { id: 1, name: 'Id (Recentes)' },
+    { id: 2, name: 'Id (Antigos)' },
+    { id: 3, name: 'Nome A-Z' },
+    { id: 4, name: 'Nome Z-A' },
+    { id: 5, name: 'Menor valor' },
+    { id: 6, name: 'Maior valor' },
+  ];
 
-  const [search, setSearch] = useState('')
-
-  const [order, setOrder] = useState<Order>('id_desc')
+  const statusOptions = [
+    { id: 'ativo', name: 'Ativos' },
+    { id: 'cancelado', name: 'Cancelados' },
+  ]
 
   // Filtro de status
   const filteredContracts = contracts.filter(contract => {
@@ -26,57 +37,74 @@ export default function App() {
         .toLowerCase()
         .includes(search.toLowerCase())
     return matchesStatus && matchesSearch
+
   }).sort((a, b) => {
     switch (order) {
 
-      case 'asc_valor':
-        return a.valor - b.valor
-
-      case 'desc_valor':
-        return b.valor - a.valor
-
-      case 'asc_alfabeto':
-        return a.cliente.localeCompare(b.cliente)
-
-      case 'desc_alfabeto':
-        return b.cliente.localeCompare(a.cliente)
-
-      case 'id_asc':
-        return a.id - b.id
-
-      case 'id_desc':
+      case 1:
         return b.id - a.id
 
+      case 2:
+        return a.id - b.id
+
+      case 3:
+        return a.cliente.localeCompare(b.cliente)
+
+      case 4:
+        return b.cliente.localeCompare(a.cliente)
+
+      case 5:
+        return a.valor - b.valor
+
+      case 6:
+        return b.valor - a.valor
+
+      default:
+        return 1;
     }
   })
+
+  const totalContractsValue = filteredContracts.reduce(
+    (total, contract) => total + contract.valor,
+    0
+  )
 
   return (
     <div>
       <h1>Dashboard de Contratos</h1>
 
       <div className="row">
+        <span className='badge badge-primary'>{filteredContracts.length} Contratos</span>
+        <br />
+        <span className='badge badge-primary'>Valor total: R${totalContractsValue}</span>
+      </div>
+
+      <div className="row">
 
         <label htmlFor="">Ordem</label>
         <select
           value={order}
-          onChange={(event) => setOrder(event.target.value as Order)}
-        >
-          <option value="id_desc">Id (Recentes)</option>
-          <option value="id_asc">Id (Antigos)</option>
-          <option value="asc_alfabeto">Nome A-Z</option>
-          <option value="desc_alfabeto">Nome Z-A</option>
-          <option value="asc_valor">Menor valor</option>
-          <option value="desc_valor">Maior valor</option>
+          onChange={(event) => setOrder(Number(event.target.value))}>
+
+          {orderOptions.map(option => (
+            <option key={option.id} value={option.id}>
+              {option.name}
+            </option>
+          ))}
+
         </select>
 
         <label htmlFor="">Status</label>
         <select
           value={statusFilter}
-          onChange={(event) => setStatusFilter(event.target.value as StatusFilter)}
-        >
+          onChange={(event) => setStatusFilter(event.target.value as StatusFilter)}>
+
           <option value="">Todos</option>
-          <option value="ativo">Ativos</option>
-          <option value="cancelado">Cancelados</option>
+          {statusOptions.map(option => (
+            <option key={option.id} value={option.id}>
+              {option.name}
+            </option>
+          ))}
         </select>
 
         <label htmlFor="">Cliente</label>
